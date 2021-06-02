391783b3169bb5fed8f6a914d00d71857fa0af80
Tim-82 <barboza-82@mail.ru>
2021-06-02 19:57:29 +0300

    created Login.vue -component

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="hadleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <div v-if="error" class="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+export default {
+  setup() {
+    const { error, login } = userLogin()
+
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value)
+      if (!error.value) {
+        console.log('user logged in')
+      }
+    }
+
+    return {email, password, handleSubmit, error}
+  }
+}
+</script>
+
+<style>
+
+</style>
